59cc8f4de0208c6128495c60a2c34f4c6599c8ff
IgriegaL <yehudilara@hotmail.com>
2023-03-15 01:13:31 -0300

    act de bd y pantallas para editar

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
+    TEMAS_CHOICES = (
+        ('Deporte', 'Deporte'),
+        ('Aire Libre', 'Aire Libre'),
+        ('VideoJuegos', 'VideoJuegos'),
+        ('Ofimatica', 'Ofimatica'),
+        ('Vacaciones', 'Vacaciones'),
+    )
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
+    tema = models.CharField(max_length=255, choices=TEMAS_CHOICES, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
